5dd9e36c66a4325b00a4805cebe73bf605cc2246
Pranab Giri <pranabgiri424@gmail.com>
2023-12-10 19:21:26 +0530

    Initial changes

--- Mul.java
+++ Mul.java
@@ -3,6 +3,6 @@ public class Mul {
         int num1 = 54;
         int num2 = 76;
         int mul = num1 * num2;
-        System.out.println("Multiply of two number is: "+ mul);
+        System.out.println("Multiply of two number is:"+ mul);
     }
 }
