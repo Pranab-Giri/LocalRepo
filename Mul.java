a6873894a791c706d1615fff991031b1dcf6289e
Pranab Giri <pranabgiri424@gmail.com>
2023-12-10 19:02:06 +0530

    add new files

---
+++ Mul.java
@@ -0,0 +1,8 @@
+public class Mul {
+    public static void main(String srg[]){
+        int num1 = 54;
+        int num2 = 76;
+        int mul = num1 * num2;
+        System.out.println("Multiply of two number is: "+ mul);
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,7 @@
+public class Sum{
+    public static void main(String arg[]){
+        int a = 90, b = 65;
+        int sum = a + b;
+        System.out.println("Sum of two given number is: "+ sum);
+    }
+}
