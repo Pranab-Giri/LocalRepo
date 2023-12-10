7e1e319dabad42e25580366c1b28996e6deee848
Pranab Giri <pranabgiri424@gmail.com>
2023-12-10 19:27:23 +0530

    intial changes in feature1

--- Sum.java
+++ Sum.java
@@ -3,5 +3,6 @@ public class Sum{
         int a = 90, b = 65;
         int sum = a + b;
         System.out.println("Sum of two given number is: "+ sum);
+        System.out.println("Sum multiply by sum :"+ sum*sum);
     }
 }
